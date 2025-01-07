e7ac0b1ab54f99357cd584e7e5211486231f2d76
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2025-01-07 11:20:10 +0530

    Added Clockin Page, Attendence Records Page and Employee Leave Requests Page.

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -5,6 +5,9 @@ import LoginPage from "./pages/LoginPage";
 import ManagerDashboard from "./pages/ManagerDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
 import EmployeeDashboard from "./pages/EmployeeDashboard";
+import EmployeeClockInPage from "./pages/EmployeeClockInPage";
+import AttendanceRecordsPage from "./pages/EmployeeAttendanceRecordsPage";
+import LeaveRequestsPage from "./pages/EmployeeLeaveRequestsPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const AppRouter: React.FC = () => {
@@ -13,7 +16,7 @@ const AppRouter: React.FC = () => {
       <Router>
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          
+
           {/* Protected routes */}
           <Route
             path="/admin/dashboard"
@@ -36,6 +39,32 @@ const AppRouter: React.FC = () => {
             element={
               <ProtectedRoute>
                 <EmployeeDashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Employee-specific routes */}
+          <Route
+            path="/employee/clockin"
+            element={
+              <ProtectedRoute>
+                <EmployeeClockInPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/employee/attendance"
+            element={
+              <ProtectedRoute>
+                <AttendanceRecordsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/employee/leave-requests"
+            element={
+              <ProtectedRoute>
+                <LeaveRequestsPage />
               </ProtectedRoute>
             }
           />
