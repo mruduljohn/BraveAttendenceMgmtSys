424a9fff966b0d04f86932e78377f8d71330e6be
aarsha01 <malu22leena@gmail.com>
2025-01-08 11:00:03 +0530

    Completed database connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'maluammu',
+        'HOST': 'localhost',  # or your database server's address
+        'PORT': '5433',       # Default PostgreSQL port
     }
 }
 
