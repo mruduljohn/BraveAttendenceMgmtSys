c5355875b004bf875d16094f304228ec5347336d
aarsha01 <malu22leena@gmail.com>
2025-01-09 16:04:04 +0530

    Changes in clocking in api

--- backend/api_app/urls.py
+++ backend/api_app/urls.py
@@ -4,5 +4,5 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('attendance/clock_in_out/', views.clock_in_out, name='clock_in_out'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
