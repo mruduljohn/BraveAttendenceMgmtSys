c478caa053995a9e348a908476b809038315cb1f
aarsha01 <malu22leena@gmail.com>
2025-01-09 15:43:42 +0530

    Added refresh token api

--- backend/api_app/urls.py
+++ backend/api_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('attendance/clock_in_out/', views.clock_in_out, name='clock_in_out'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
